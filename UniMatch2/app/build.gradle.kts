f2fa1019a1b23debbfd0059cd6fcd0253b2a9fd4
Diego Tolosa <tolosad83@gmail.com>
2025-04-30 17:01:42 -0500

    se agregaron algunas configuraciones y la view del login

--- UniMatch2/app/build.gradle.kts
+++ UniMatch2/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+
 }
 
 android {
@@ -9,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.univalle.unimatch"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -33,12 +35,50 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
-    implementation("com.lorentzos.swipecards:library:1.0.9@aar")
+
+    // Import the Firebase BoM
+    implementation(platform(libs.firebase.bom))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation(libs.firebase.analytics)
+
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+
+    implementation (libs.firebase.auth) // Firebase Auth
+    implementation (libs.androidx.lifecycle.viewmodel.ktx) // ViewModel
+    implementation (libs.androidx.lifecycle.livedata.ktx)  // LiveData
+    implementation (libs.androidx.lifecycle.viewmodel.compose) // ViewModel para Jetpack Compose
+
+    implementation (libs.material3)
+
+    implementation(libs.androidx.navigation.compose.v275)
+
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
--- UniMatch2/app/src/main/java/com/univalle/unimatch/MainActivity.kt
+++ UniMatch2/app/src/main/java/com/univalle/unimatch/MainActivity.kt
@@ -1,97 +1,36 @@
 package com.univalle.unimatch
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import com.lorentzos.swipecards.SwipeFlingAdapterView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.univalle.unimatch.ui.theme.UvMatchTheme
+import com.univalle.unimatch.presentation.view.LoginScreen
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var flingContainer: SwipeFlingAdapterView
-    private lateinit var al: ArrayList<String>
-    private lateinit var arrayAdapter: ArrayAdapter<String>
-    private var i: Int = 0
 
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            UvMatchTheme (){
+                val navController = rememberNavController() // Inicializamos NavController
+                Scaffold (modifier = Modifier.fillMaxSize()){ innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login_screen",
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable("login_screen") { LoginScreen(navController) }
+                    }
 
-        // Inicializa tu SwipeFlingAdapterView (asumiendo que lo tienes en tu layout)
-        flingContainer = findViewById(R.id.frame)
-
-        // Inicializa el ArrayList con datos
-        al = ArrayList()
-        al.add("php")
-        al.add("c")
-        al.add("python")
-        al.add("java")
-        al.add("html")
-        al.add("c++")
-        al.add("css")
-        al.add("javascript")
-
-        // Configura el adaptador
-        arrayAdapter = ArrayAdapter(this, R.layout.item, R.id.helloText, al)
-        flingContainer.adapter = arrayAdapter
-
-        // Configura el listener para los eventos de swipe
-        flingContainer.setFlingListener(object : SwipeFlingAdapterView.onFlingListener {
-            override fun removeFirstObjectInAdapter() {
-                // Esta es la forma más simple de eliminar un objeto del adaptador
-                Log.d("LIST", "removed object!")
-                al.removeAt(0)
-                arrayAdapter.notifyDataSetChanged()
+                }
             }
-
-            override fun onLeftCardExit(dataObject: Any) {
-                // Hacer algo con el swipe hacia la izquierda
-                makeToast(this@MainActivity, "Left!")
-            }
-
-            override fun onRightCardExit(dataObject: Any) {
-                makeToast(this@MainActivity, "Right!")
-            }
-
-            override fun onAdapterAboutToEmpty(itemsInAdapter: Int) {
-                // Solicitar más datos aquí
-                al.add("XML ".plus(i.toString()))
-                arrayAdapter.notifyDataSetChanged()
-                Log.d("LIST", "notified")
-                i++
-            }
-
-            override fun onScroll(scrollProgressPercent: Float) {
-                val view = flingContainer.selectedView
-                view.findViewById<View>(R.id.item_swipe_right_indicator).alpha =
-                    if (scrollProgressPercent < 0) -scrollProgressPercent else 0f
-                view.findViewById<View>(R.id.item_swipe_left_indicator).alpha =
-                    if (scrollProgressPercent > 0) scrollProgressPercent else 0f
-            }
-        })
-
-        // Opcionalmente añadir un OnItemClickListener
-        flingContainer.setOnItemClickListener { itemPosition, dataObject ->
-            makeToast(this@MainActivity, "Clicked!")
         }
-    }
-
-    companion object {
-        fun makeToast(ctx: Context, s: String) {
-            Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    // Si estás usando alguna librería de inyección como ButterKnife en Kotlin,
-    // deberías reemplazar estos métodos con la alternativa adecuada
-    fun right() {
-        // Trigger the right event manually
-        flingContainer.topCardListener.selectRight()
-    }
-
-    fun left() {
-        flingContainer.topCardListener.selectLeft()
     }
 }
