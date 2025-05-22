7c9ac4e142cde486e43f986caad7e90728128b6f
Sebas <joan.saavedra@correounivalle.edu.co>
2025-05-21 21:18:56 -0500

    feat:account verification logic
    verification is sent to firebase.

--- UniMatch2/app/src/main/java/com/univalle/unimatch/data/repository/AuthRepository.kt
+++ UniMatch2/app/src/main/java/com/univalle/unimatch/data/repository/AuthRepository.kt
@@ -6,50 +6,60 @@ import com.google.firebase.firestore.FirebaseFirestore
 class AuthRepository(
     private val auth: FirebaseAuth = FirebaseAuth.getInstance(),
     private val firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
-)
- {
-     fun registerUser(
-         email: String,
-         password: String,
-         nombre: String,
-         apellido: String,
-         nombreCuenta: String,
-         phoneNumber: String,
-         birthDate: String,
-         gender: String,
-         onSuccess: () -> Unit,
-         onError: (String) -> Unit,
-     ) {
-         auth.createUserWithEmailAndPassword(email, password)
-             .addOnCompleteListener { task ->
-                 if (task.isSuccessful) {
-                     val user = auth.currentUser
-                     val uid = user?.uid
+) {
+    fun registerUser(
+        email: String,
+        password: String,
+        nombre: String,
+        apellido: String,
+        nombreCuenta: String,
+        phoneNumber: String,
+        birthDate: String,
+        gender: String,
+        onSuccess: () -> Unit,
+        onError: (String) -> Unit,
+    ) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    val uid = user?.uid
 
-                     val userData = mapOf(
-                         "uid" to uid,
-                         "nombre" to nombre,
-                         "apellido" to apellido,
-                         "nombreCuenta" to nombreCuenta,
-                         "email" to email,
-                         "phoneNumber" to phoneNumber,
-                         "birthDate" to birthDate,
-                         "gender" to gender,
-                         "verified" to false
-                     )
+                    val userData = mapOf(
+                        "uid" to uid,
+                        "nombre" to nombre,
+                        "apellido" to apellido,
+                        "nombreCuenta" to nombreCuenta,
+                        "email" to email,
+                        "phoneNumber" to phoneNumber,
+                        "birthDate" to birthDate,
+                        "gender" to gender,
+                        "verified" to false
+                    )
 
-                     uid?.let {
-                         firestore.collection("users")
-                             .document(it)
-                             .set(userData)
-                             .addOnFailureListener { e ->
-                                 onError("Error al guardar datos en Firestore: ${e.message}")
-                             }
-                     }
-                 } else {
-                     onError("Error al registrar usuario: ${task.exception?.message}")
-                 }
-             }
-         }
-     fun getCurrentUser() = auth.currentUser
+                    uid?.let {
+                        firestore.collection("users")
+                            .document(it)
+                            .set(userData)
+                            // se utiliza para enviar el correo de verificacion
+                            .addOnSuccessListener {
+                                user.sendEmailVerification().addOnCompleteListener { emailTask ->
+                                    if (emailTask.isSuccessful) {
+                                        onSuccess()
+                                    } else {
+                                        onError("Error al enviar correo de verificación: ${emailTask.exception?.message}")
+                                    }
+                                }
+                            }
+                            .addOnFailureListener { e ->
+                                onError("Error al guardar datos en Firestore: ${e.message}")
+                            }
+                    }
+                } else {
+                    onError("Error al registrar usuario: ${task.exception?.message}")
+                }
+            }
+    }
+
+    fun getCurrentUser() = auth.currentUser
 }
