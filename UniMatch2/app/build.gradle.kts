5a1f6a77040d2c93c7aabe32f61c751d5c0699c8
Diego Tolosa <tolosad83@gmail.com>
2025-06-19 00:05:04 -0500

    feat: agregar lógica para subir fotos y mejorar la navegación entre pantallas

--- UniMatch2/app/build.gradle.kts
+++ UniMatch2/app/build.gradle.kts
@@ -101,6 +101,7 @@ dependencies {
     implementation(libs.firebase.auth.ktx)
     implementation(libs.androidx.navigation.runtime.android)
     implementation (libs.landscapist.coil)
+    implementation(libs.firebase.storage.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
